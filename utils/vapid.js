54a630379cbacbe19f092000cb64e4a4a6fef4bc
maejima <i10784@nara.kosen-ac.jp>
2025-05-04 15:36:18 +0900

    debug

--- utils/vapid.js
+++ utils/vapid.js
@@ -1,5 +1,3 @@
 // utils/vapid.js
-module.exports = {
-  publicKey: 'BDkRXwXv1YXET20TXa-Tg0Mz2zuJhwIG6beU4OH58_kHa39HZSKsTT5GDEuMnwLwcMGK0hgBH830OTNHGmbj5lo',
-  privateKey: 'EZtuEs6no_7FYjGJXh5OWDH6QFCocM1sHk3klxXvfLo'
-};
+export const publicKey = 'BDkRXwXv1YXET20TXa-Tg0Mz2zuJhwIG6beU4OH58_kHa39HZSKsTT5GDEuMnwLwcMGK0hgBH830OTNHGmbj5lo';
+export const privateKey = 'EZtuEs6no_7FYjGJXh5OWDH6QFCocM1sHk3klxXvfLo';
